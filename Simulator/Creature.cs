535e6c0c1fb6cdec960096686d745597d6d189e4
Splafty <Splaft@wp.pl>
2024-10-29 20:20:10 +0100

    Settery

--- Simulator/Creature.cs
+++ Simulator/Creature.cs
@@ -3,18 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Simulator;
 
 public class Creature
 {
     // Properties
-    private string? name;
-    public string? Name { get; }
+    private string name = "Unknown";
+    public string Name 
+    {
+        get => name;
+        init
+        {
+            value = value.Trim();
+
+            if (value.Length > 25)
+            {
+                value = value.Substring(0, 25).TrimEnd();
+            }
+
+            if (value.Length < 3)
+            {
+                value = value.PadRight(3, '#');
+            }
+
+            if (char.IsLower(value[0]))
+            {
+                value = char.ToUpper(value[0]) + value.Substring(1);
+            }
+
+            name = value;
+        }
+    }
 
 
     private int level;
-    public int Level { get; set; }
+    public int Level
+    {
+        get => level;
+        init
+        {
+
+            if (value < 1)
+            {
+                value = 1;
+            }
+
+            else if (value > 10)
+            {
+                value = 10;
+            }
+
+            level = value;
+        }
+    }
 
 
     // Constructors
@@ -31,4 +74,12 @@ public class Creature
     public void SayHi() => Console.WriteLine($"Hi, I'm {Name}, my level is {Level}.");
 
     public string Info => $"{Name} [{Level}]";
+
+    public void Upgrade()
+    {
+        if (level < 10)
+        {
+            level++;
+        }
+    }
 }
--- Simulator/Program.cs
+++ Simulator/Program.cs
@@ -5,25 +5,62 @@ internal class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Starting Simulator!\n");
+        Lab3a();
 
-        Creature c = new Creature("Shrek", 7);
+        //    Early tests
+        //    Creature c = new Creature("Shrek", 7);
+        //    c.SayHi();
+        //    Console.WriteLine(c.Info);
+
+        //    Creature d = new Creature("Fiona");
+        //    d.SayHi();
+        //    Console.WriteLine(d.Info);
+
+        //    Creature e = new Creature();
+        //    e.SayHi();
+        //    Console.WriteLine(d.Info);
+
+        //    Animals rats = new() { Description = "Rats", Size = 10 };
+        //    Console.WriteLine(rats.Info);
+        //    Animals mice = new() { Description = "Mice", Size = 40 };
+        //    Console.WriteLine(mice.Info);
+        //    Animals cats = new() { Description = "Cats" }; // cats.Size == 3
+        //    Console.WriteLine(cats.Info);
+    }
+
+
+    // Copied function
+    static void Lab3a()
+    {
+        Creature c = new() { Name = "   Shrek    ", Level = 20 };
         c.SayHi();
-        c.Level = 8;
+        c.Upgrade();
         Console.WriteLine(c.Info);
 
-        Creature d = new Creature("Fiona");
-        d.SayHi();
-        Console.WriteLine(d.Info);
+        c = new("  ", -5);
+        c.SayHi();
+        c.Upgrade();
+        Console.WriteLine(c.Info);
 
-        Creature e = new Creature();
-        e.SayHi();
-        Console.WriteLine(d.Info);
+        c = new("  donkey ") { Level = 7 };
+        c.SayHi();
+        c.Upgrade();
+        Console.WriteLine(c.Info);
 
-        Animals rats = new() { Description = "Rats", Size = 10 };
-        Console.WriteLine(rats.Info);
-        Animals mice = new() { Description = "Mice", Size = 40 };
-        Console.WriteLine(mice.Info);
-        Animals cats = new() { Description = "Cats" }; // cats.Size == 3
-        Console.WriteLine(cats.Info);
+        c = new("Puss in Boots – a clever and brave cat.");
+        c.SayHi();
+        c.Upgrade();
+        Console.WriteLine(c.Info);
+
+        c = new("a                            troll name", 5);
+        c.SayHi();
+        c.Upgrade();
+        Console.WriteLine(c.Info);
+
+        var a = new Animals() { Description = "   Cats " };
+        Console.WriteLine(a.Info);
+
+        a = new() { Description = "Mice           are great", Size = 40 };
+        Console.WriteLine(a.Info);
     }
 }
